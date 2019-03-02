1dbe0666fe71571cf4aa0eb8aeb621af7838a322
Mathis Van Eetvelde <Mathisco-01@users.noreply.github.com>
2019-03-02 21:00:22 +0100

    Added channel ID input

--- discordDir/discordBot.py
+++ discordDir/discordBot.py
@@ -2,12 +2,12 @@ import json
 import requests
 import os
 
-def getToken(pathToToken):
+def getConfigData(path):
 	try:
-		with open(pathToToken, 'r') as file:
+		with open(path, 'r') as file:
 			for line in file:
-				discord_token = line.replace("\n", "")
-		return discord_token
+				data = line.replace("\n", "")
+		return data
 	except:
 		return ""
 		
@@ -15,17 +15,30 @@ def makeToken(pathToToken):
 	with open(pathToToken, 'w+') as file:
 		file.write(input("Paste your dicord token here: "))
 
+def makeChannel(pathToChannel):
+	with open(pathToChannel, 'w+') as file:
+		file.write(input('Paste your channel ID here: '))
+
 def fetchToken():
 	pathToToken = os.path.join(os.getcwd(),'discordDir', 'token.txt')
-	token = getToken(pathToToken)
+	token = getConfigData(pathToToken)
 	if(token == "" or token == None):
 		makeToken(pathToToken)
-		fetchToken(pathToToken)
+		fetchToken()
 	else:
 		return token
 
+def fetchChannel():
+	pathToChannel = os.path.join(os.getcwd(), 'discordDir', 'channel.txt')
+	channel = getConfigData(pathToChannel)
+	if(channel == "" or channel == None):
+		makeChannel(pathToChannel)
+		fetchChannel()
+	return channel
+
+
 def sendMessage(name,commit, url):
-	baseURL = "https://discordapp.com/api/channels/{}/messages".format('550724065581596684')
+	baseURL = "https://discordapp.com/api/channels/{}/messages".format(str(fetchChannel()))
 
 	string = "@here {} Just got a new commit: __**{}**__! {}".format(name, commit, url)
 	POSTedJSON =  json.dumps({"content":string})
--- main.py
+++ main.py
@@ -78,4 +78,5 @@ def main():
 if __name__ == '__main__':
 	while True:
 		main()
+		print("Done")
 		time.sleep(10)
